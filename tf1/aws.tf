1bfc6f9c68e0ad36d09f58b4d6a4a2df10f31828
Maytham Alherz <hmaytham@hotmail.com>
2021-08-04 08:16:59 +0200

    another small comment

--- tf1/aws.tf
+++ tf1/aws.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = var.region
 }
 
-# just a simple ec2 instance
+# just a simple ec2 instance that will run in the default vpc
 resource "aws_instance" "a" {
   ami           = var.ami
   instance_type = var.instance_type
@@ -12,6 +12,7 @@ resource "aws_instance" "a" {
   }
 
 }
+# just a simple privet s3 bucket 
 resource "aws_s3_bucket" "a" {
   bucket = "flugel"
   acl    = "private"
