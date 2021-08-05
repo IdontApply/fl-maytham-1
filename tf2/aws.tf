9d63077988b992faf34e5ff7582cf2b2c2f4f214
Maytham Alherz <hmaytham@hotmail.com>
2021-08-05 17:39:42 +0200

    change remote-exec provisioner for user_data try 1

--- tf2/aws.tf
+++ tf2/aws.tf
@@ -251,28 +251,27 @@ resource "aws_instance" "b" {
   }
 
   user_data = << EOF
-  #!/usr/bin/bash
-  set -x
-  sudo apt update -y
-  sudo apt -y upgrade
-  sudo apt install nginx -y
-  sudo ufw allow 'Nginx HTTP'
+    #!/usr/bin/bash
+    set -x
+    sudo apt update -y
+    sudo apt -y upgrade
+    sudo apt install nginx -y
+    sudo ufw allow 'Nginx HTTP'
 
-  sudo mkdir -p /var/www/domain/html
-  sudo chown -R $USER:$USER /var/www/domain/html
-  sudo chmod -R 755 /var/www/domain
+    sudo mkdir -p /var/www/domain/html
+    sudo chown -R $USER:$USER /var/www/domain/html
+    sudo chmod -R 755 /var/www/domain
 
-  sudo chmod +x /tmp/nginx.py
-  sudo python3 /tmp/nginx.py ${var.tag_name}
+    sudo chmod +x /tmp/nginx.py
+    sudo python3 /tmp/nginx.py ${var.tag_name}
 
-  yes | sudo cp /tmp/index.html /var/www/domain/html
-  yes | sudo cp /tmp/index.html /var/www/html/index.nginx-debian.html
-  yes | sudo cp /tmp/domain /etc/nginx/sites-available
+    yes | sudo cp /tmp/index.html /var/www/domain/html
+    yes | sudo cp /tmp/index.html /var/www/html/index.nginx-debian.html
+    yes | sudo cp /tmp/domain /etc/nginx/sites-available
 
-  sudo ln -s /etc/nginx/sites-available/domain /etc/nginx/sites-enabled/
-  sudo systemctl restart nginx
-  set +x
-
+    sudo ln -s /etc/nginx/sites-available/domain /etc/nginx/sites-enabled/
+  	sudo systemctl restart nginx
+  	set +x
 	EOF
   # provisioner "remote-exec" {
   #   on_failure = “continue”
